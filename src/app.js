60ba2bdb8f88a244cebecf581078014eac5b9754
Varun Kumar <vkumar5@paypal.com>
2019-08-30 15:22:57 +0100

    Correct to PORT in Caps for Heroku

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const express = require('express')
 const hbs = require('hbs')
 
 const app = express()
-const port = process.env.port || 3000
+const port = process.env.PORT || 3000
 
 const geocode = require ('./utils/geocode')
 const forecast = require ('./utils/forecast')
